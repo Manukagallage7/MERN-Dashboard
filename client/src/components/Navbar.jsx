64ad2edb55040d06aa767a85eb747a65cfc74fc1
Manukagallage7 <manukakavinda1110@gmail.com>
2025-09-17 20:08:12 +0530

    Refactor Navbar component to accept sidebar state as props; add user model with validation schema

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,10 @@ import { useDispatch } from 'react-redux';
 import { setMode } from '../state';
 import {AppBar, IconButton, InputBase, Toolbar, useTheme} from "@mui/material";
 
-const Navbar = () => {
+const Navbar = ({
+    isSidebarOpen,
+    setIsSidebarOpen,
+}) => {
     const dispatch = useDispatch();
     const theme = useTheme();
     return (
@@ -20,7 +23,7 @@ const Navbar = () => {
             <Toolbar sx={{ justifyContent: "space-between" }}>
                 {/* LEFT SIDE */}
                 <FlexBetween>
-                    <IconButton onClick={() => console.log("open/close sidebar")}>
+                    <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
                         <MenuIcon />
                     </IconButton>
                         <FlexBetween
