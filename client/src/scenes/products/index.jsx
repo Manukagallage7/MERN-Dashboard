c28723fe443ebd74130c855ed41656b64b35f93c
Manukagallage7 <manukakavinda1110@gmail.com>
2025-09-18 22:53:53 +0530

    Implement product retrieval and enhance Header component for product display

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import {useState} from "react";
+import { useGetProductsQuery } from "state/api";
+import Header from "components/Header"
+import {BOx, Card, CardActions, CardContent, Collapse, Button, Typography, Rating, UseTheme, useMediaQuery} from "@mui/material";
+
 
 const Products = ()=>  {
+    const {data , isLoading} = useGetProductsQuery();
+    const isNonMobile = 
+    console.log("data", data)
+
     return (
-            <div>
-                Layout
-            </div>
+            <box>
+                <Header title="PRODUCTS" subtitle="See your list of products." />
+            </box>
         )
 }
 
