6b35ced093c7ee31382d726a47641c7ad6282e84
Manukagallage7 <manukakavinda1110@gmail.com>
2025-09-20 20:53:34 +0530

    Enhance Customers scene: format phone numbers and improve DataGrid styling

--- client/src/scenes/customers/index.jsx
+++ client/src/scenes/customers/index.jsx
@@ -26,8 +26,26 @@ const Customers = () => {
             flex: 1,
         },
         {
-            field: "",
-            headerName: "ID",
+            field: "phoneNumber",
+            headerName: "Phone Number",
+            flex: 0.5,
+            renderCell: (params)=> {
+                return params.value.replace(/^(\d{3})(\d{3})(\d{4})/, "($1)$2-$3")
+            }
+        },
+        {
+            field: "country",
+            headerName: "Country",
+            flex: 0.4,
+        },
+        {
+            field: "occupation",
+            headerName: "Occupation",
+            flex: 0.5,
+        },
+        {
+            field: "role",
+            headerName: "Role",
             flex: 1,
         }
     ]
@@ -35,10 +53,37 @@ const Customers = () => {
     return(
         <Box m="1.5rem 2.5rem">
             <Header title="CUSTOMERS" subtitle="List of Customers" />
-            <Box>
+            <Box
+                mt="40px"
+                height="75vh"
+                sx={{
+                    "& .MuiDataGrid-root": {
+                        border: "none"
+                    },
+                    "& .MuiDataGrid-cell": {
+                        borderBottom: "none"
+                    },
+                    "& .MuiDataGrid-columnHeaders": {
+                        backgroundColor: theme.palette.background.alt,
+                        color: theme.palette.secondary[100],
+                        borderBottom: "none"
+                    },
+                    "& .MuiDataGrid-virtualScroller": {
+                        backgroundColor: theme.palette.primary.light,
+                    },
+                    "& .MuiDataGrid-footerContainer": {
+                        backgroundColor: theme.palette.background.alt,
+                        color: theme.palette.secondary[100],
+                        borderTop: "none",
+                    },
+                    "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                        color: `${theme.palette.secondary[200]} !important`
+                    }
+                }}
+            >
                 <DataGrid
                     loading={isLoading || !data}
-                    getRowId={(row) =>}
+                    getRowId={(row) => row._id}
                     rows={data || []}
                     columns={columns}
                 />
